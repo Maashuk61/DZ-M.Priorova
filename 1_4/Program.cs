cbf404be084dbe42f6db3c4a8f52f020ab08cf4d
Maria <mpriorova@mail.ru>
2022-12-21 01:47:20 +0300

    Написали программу, которая находит четные числа от 1 до N

---
+++ 1_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine ());
+
+int i = 1;
+
+Console.WriteLine($"Четные числа от 1 до {number}: ");
+
+while (i <= number)
+{ 
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}, ");
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
