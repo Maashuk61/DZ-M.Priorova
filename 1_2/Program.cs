f6af79677fc0806b2f52bd6a82c82fb88452fe03
Maria <mpriorova@mail.ru>
2022-12-21 00:24:47 +0300

    Написали программу, которая находит максимум из трех чисел

---
+++ 1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа,
+// и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Введите первое число:");
+int firstnumber = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите второе число:");
+int secondnumber = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите третье число:");
+int thirdnumber = Convert.ToInt32(Console.ReadLine ());
+
+int max = firstnumber;
+if (firstnumber < secondnumber) max = secondnumber;
+if (secondnumber < thirdnumber) max = thirdnumber;
+
+ Console.WriteLine (max);
