dda7ec5b433630ab86a1578d29cd7b71179ea01e
Maria <mpriorova@mail.ru>
2022-12-20 23:56:03 +0300

    first commit

---
+++ 1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число:");
+int firstnumber = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите второе число:");
+int secondnumber = Convert.ToInt32(Console.ReadLine ());
+if (firstnumber > secondnumber)
+{
+    Console.WriteLine (firstnumber);
+}
+else
+{
+    Console.WriteLine (secondnumber);
+}
